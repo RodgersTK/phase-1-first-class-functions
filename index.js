2383d33f7ba0b13f9b6151c1d70574c887521001
RodgersTK <rodgerstkm@gmail.com>
2024-03-24 00:21:49 +0300

    returning a function

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      // Function body
+    };
+  }
+  function returnsAnAnonymousFunction() {
+    return function() {
+      // Function body
+    };
+  }
